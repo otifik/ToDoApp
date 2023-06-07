6ab6af0968d1b618f0a8dfa300baa44b2d993635
autumn-linux <3078496627@qq.com>
2023-06-07 15:38:46 +0800

    2.根据https://www.youtube.com/watch?v=yMGAbm84iIY修改了Proto DataStore写法，优化了目录与类结构(According to https://www.youtube.com/watch?v=yMGAbm84iIY, the writing method of Proto DataStore has been modified, and the directory and class structure have been optimized.)

--- app/src/main/java/xyz/otifik/todoapp/compose/TodoCompose.kt
+++ app/src/main/java/xyz/otifik/todoapp/compose/TodoCompose.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,6 +23,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 //@Preview
 fun ToDoContent() {
+    
     LazyColumn(content = {
         item {
             for (i in 0..10) {
--- app/src/main/java/xyz/otifik/todoapp/repository/LocalRepository.kt
+++ app/src/main/java/xyz/otifik/todoapp/repository/LocalRepository.kt
@@ -1,12 +1,2 @@
 package xyz.otifik.todoapp.repository
 
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "TodoRepo")
-class LocalRepository {
-
-}
